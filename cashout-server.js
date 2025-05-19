bc27c35b5d60384a1067417516146c1fa504cf1b
Thearbinaut05 <thearbinaut05@gmail.com>
2025-05-18 19:44:43 -0700

    Update cashout-server.js

--- cashout-server.js
+++ cashout-server.js
@@ -1,5 +1,5 @@
 [!DOCTYPE HTML]
-Cashout-server_stripe.js<=
+Cashout-server.js<=
 // 1. First, add Stripe.js to your HTML
 // Add this to your HTML head section:
 // <script src="https://js.stripe.com/v3/"></script>
